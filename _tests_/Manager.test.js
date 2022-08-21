6e3f985068ce24fec9a3c21a4c485eb3162eb325
aurumbright <aurum.elean@gmail.com>
2022-08-20 17:38:48 -0700

    tests written and passed

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,5 +1,22 @@
-const Employee = require('./lib/Employee');
-const Manager = require('./lib/Manager');
+const Manager = require('../lib/Manager');
 
+describe("Manager", () => {
+    it("Can set office number from constructor", () => {
+        const officeNum = "Mithraeum";
+        const employee = new Manager("John", 1, "emperor@first.com", officeNum);
+        expect(employee.officeNumber).toBe(officeNum);
+    });
 
+    it("Can get office number username via getOfficeNumber()", () => {
+        const officeNum = "Mthraeum";
+        const employee = new Manager("John", 1, "emperor@first.com", officeNum);
+        expect(employee.getOfficeNumber()).toBe(officeNum);
+    });
+
+    it("getRole() should return \"Manager\"", () => {
+        const role = "Manager";
+        const employee = new Manager("John", 1, "emperor@first.com", "Mithraeum");
+        expect(employee.getRole()).toBe(role);
+    });
+});
 
